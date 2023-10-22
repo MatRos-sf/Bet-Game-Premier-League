9699fceb282d6b07cfb26584554c0d36ac9142fb
MatRos-sf <mateuszrosenkranz@gmail.com>
2023-10-22 12:15:49 +0200

    feat(bet.modrl) Create model Bet

--- BetGame_PremierLeague/bet/models.py
+++ BetGame_PremierLeague/bet/models.py
@@ -4,7 +4,13 @@ from django.utils import timezone
 
 class Bet(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    match = models.ForeignKey('match.Match', on_delete=models.CASCADE)
+    match = models.OneToOneField('match.Match', on_delete=models.CASCADE)
     date_of_bet = models.DateTimeField(default=timezone.now)
     date_of_match = models.DateTimeField(blank=True, null=True)
+
+    home_team = models.ManyToManyField(User)
+    draw = models.ManyToManyField(User)
+    away_team = models.ManyToManyField(User)
+
+    def all_bet(self):
+        pass
