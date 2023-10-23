915757949104de7b221d01bc8ca8668728ab4c7f
MatRos-sf <mateuszrosenkranz@gmail.com>
2023-10-23 13:37:43 +0200

    feat(users.signals): modify create_profile_and_season_points

--- BetGame_PremierLeague/users/signals.py
+++ BetGame_PremierLeague/users/signals.py
@@ -2,13 +2,19 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
-from .models import Profile
+from .models import Profile, SeasonPoints
+from league.models import Season
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_profile_and_season_points(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        profile = Profile.objects.create(user=instance)
+        seasons = Season.objects.filter(is_currently=True)
+
+        if seasons.exists():
+            for season in seasons:
+                SeasonPoints.objects.create(profile=profile, seasons=season)
 
 
 # @receiver(post_save, sender=User)
