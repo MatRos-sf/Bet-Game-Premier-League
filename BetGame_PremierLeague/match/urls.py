c2aecfb7972bb1ede2b701d118d4925c225402b6
MatRos-sf <mateuszrosenkranz@gmail.com>
2023-11-02 10:56:06 +0100

    feat(match) add results

--- BetGame_PremierLeague/match/urls.py
+++ BetGame_PremierLeague/match/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import MatchDetailView
+from .views import MatchDetailView, ResultsSeasonListView
 
-urlpatterns = [path("<int:pk>/", MatchDetailView.as_view(), name="match-detail")]
+urlpatterns = [
+    path("<int:pk>/", MatchDetailView.as_view(), name="match-detail"),
+    path("results/", ResultsSeasonListView.as_view(), name="match-season-results"),
+]
--- BetGame_PremierLeague/match/views.py
+++ BetGame_PremierLeague/match/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Match, Matchweek
@@ -17,4 +17,25 @@ class MatchDetailView(LoginRequiredMixin, DetailView):
         table = TeamStats.get_season_table(season=season.year, league=league)
         context["table"] = table
         # TODO takie staty jak tu: https://www.premierleague.com/match/93424
+        return context
+
+
+class ResultsSeasonListView(LoginRequiredMixin, ListView):
+    model = Match
+    template_name = "match/results.html"
+
+    def get_queryset(self):
+        queryset = self.model.objects.filter(
+            finished=True, matchweek__season__is_currently=True
+        )
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ResultsSeasonListView, self).get_context_data(*kwargs)
+
+        # season
+        match = context["object_list"].first()
+        season_name = f"{match.matchweek.season.start_date.strftime('%Y')}/{match.matchweek.season.end_date.strftime('%y')}"
+        context["season_name"] = season_name
+
         return context
