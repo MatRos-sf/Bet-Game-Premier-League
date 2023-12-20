3ae7bd13d92d81c8a65d958c8f3e571b37b8e390
MatRos-sf <mateuszrosenkranz@gmail.com>
2023-12-20 23:59:54 +0100

    add charts meethod

--- BetGame_PremierLeague/bet/urls.py
+++ BetGame_PremierLeague/bet/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 
-from .views import BetsListView, UserFinishedBetsListView
+from .views import BetsListView, UserFinishedBetsListView, BetSeasonSummaryView
 
 urlpatterns = [
     path("", BetsListView.as_view(), name="bet-home"),
     path("finished/", UserFinishedBetsListView.as_view(), name="bet-finished"),
+    path(
+        "summary/<int:year>/", BetSeasonSummaryView.as_view(), name="bet-season-summary"
+    ),
 ]
--- BetGame_PremierLeague/bet/views.py
+++ BetGame_PremierLeague/bet/views.py
@@ -1,12 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count, Q, Max
 from django.contrib import messages
 from django.db.models import QuerySet
 from django.utils import timezone
-from django.shortcuts import render, HttpResponse
-
+from typing import Any
 from match.models import Match, Matchweek
-from .models import Bet
+from .models import Bet, Dict
 
 
 class BetsListView(LoginRequiredMixin, ListView):
@@ -70,8 +70,6 @@ class BetsListView(LoginRequiredMixin, ListView):
             .get(pk=match_pk)
         )
 
-        s = match.matchweek.start_date
-        t = timezone.now().date()
         if timezone.now().date() < match.matchweek.start_date:
             bet, _ = Bet.objects.get_or_create(
                 match=Match.objects.get(pk=match_pk), user=request.user
@@ -109,3 +107,96 @@ class UserFinishedBetsListView(LoginRequiredMixin, ListView):
             .prefetch_related("match", "match__away_team", "match__home_team")
             .order_by("-match__start_date")
         )
+
+
+class BetSeasonSummaryView(LoginRequiredMixin, ListView):
+    model = Bet
+    template_name = "bet/bet_season_summary.html"
+
+    def get_queryset(self) -> QuerySet[Bet]:
+        return Bet.objects.filter(
+            match__matchweek__start_date__year=2023, is_active=False
+        ).only("is_won", "risk", "choice", "is_won", "match__matchweek__matchweek")
+
+    def __create_pie_chart(self, labels: list, values: list):
+        import plotly.graph_objects as go
+
+        fig = go.Figure(
+            data=[
+                go.Pie(
+                    labels=labels,
+                    values=values,
+                    textinfo="label+percent",
+                    insidetextorientation="radial",
+                )
+            ]
+        )
+        return fig
+
+    def __create_bar_chart(self, labels: list, values: list):
+        import plotly.graph_objects as go
+
+        fig = go.Figure([go.Bar(x=labels, y=values)])
+
+        return fig
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super(BetSeasonSummaryView, self).get_context_data(**kwargs)
+        bet_list = context["object_list"]
+        bets = bet_list.aggregate(
+            amt_bet_risk=Count("risk", filter=Q(risk=True)),
+            amt_bet=Count("pk"),
+            win_bets=Count("is_won", filter=Q(is_won=True)),
+            lose_bets=Count("is_won", filter=Q(is_won=False)),
+            home_bet=Count("choice", filter=Q(choice="home")),
+            away_bet=Count("choice", filter=Q(choice="away")),
+            draw_bet=Count("choice", filter=Q(choice="draw")),
+            none_bet=Count("choice", filter=Q(choice="none")),
+            max_matchweeks=Max("match__matchweek__matchweek"),
+        )
+
+        print(bets["max_matchweeks"])
+        # pie chart with kind of bets
+        amt_bet = bets["amt_bet"]
+        amt_bet_risk = bets["amt_bet_risk"]
+        pie = self.__create_pie_chart(
+            ["bets without risk", "risk bets"], [amt_bet - amt_bet_risk, amt_bet_risk]
+        )
+        context["chart_kind_of_bets"] = pie.to_html()
+
+        # pie chart with win and loses
+        pie = self.__create_pie_chart(
+            ["won bets", "lost bets"], [bets["win_bets"], bets["lose_bets"]]
+        )
+        context["chart_won_lost"] = pie.to_html()
+
+        # Bar Charts
+        chart = self.__create_bar_chart(
+            ["home", "draw", "away"],
+            [bets["home_bet"], bets["draw_bet"], bets["away_bet"]],
+        )
+        context["chart_choiced"] = chart.to_html()
+
+        # group bar chart
+        import plotly.graph_objects as go
+
+        list_of_matchweek = list(range(1, bets["max_matchweeks"] + 1))
+        query_dict = {}
+        for i in list_of_matchweek:
+            filter_key = f"matchweek_{i}"
+            query_dict[filter_key] = Count(
+                "pk", filter=Q(match__matchweek__matchweek=i)
+            )
+
+        q = bet_list.aggregate(**query_dict)
+
+        fig = go.Figure(
+            data=[
+                go.Bar(name="", x=list(q.keys()), y=list(q.values())),
+                # go.Bar(name='LA Zoo', x=list_of_matchweek, y=[12, 18, 29])
+            ]
+        )
+        # Change the bar mode
+        # fig.update_layout(barmode='stack')
+        context["chart_group"] = fig.to_html()
+        return context
