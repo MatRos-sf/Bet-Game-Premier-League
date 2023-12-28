ac930b41e604d803f7ee83da50380eeb021e12d9
MatRos-sf <mateuszrosenkranz@gmail.com>
2023-12-28 09:39:19 +0100

    add new kind field to UserScores

--- BetGame_PremierLeague/bet/signals.py
+++ BetGame_PremierLeague/bet/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from notifications.signals import notify
 
@@ -18,6 +18,7 @@ def allocation_point_for_won_bet(sender, instance: Bet, **kwargs) -> None:
             profile=instance.user.profile,
             points=points,
             description=UserScores.render_description(points, f"WON bet {instance.pk}"),
+            kind=UserScores.Kind.BET,
         )
         notify.send(
             instance.match,
@@ -25,9 +26,7 @@ def allocation_point_for_won_bet(sender, instance: Bet, **kwargs) -> None:
             verb=f"finished; you won {points} pts.",
         )
     elif isinstance(instance.is_won, bool) and not instance.is_won:
-        notify.send(
-            instance.match, recipient=instance.user, verb=f"finished; you lost."
-        )
+        notify.send(instance.match, recipient=instance.user, verb="finished; you lost.")
 
 
 @receiver(post_save, sender=Bet)
@@ -40,4 +39,5 @@ def capture_risk_bet(sender, instance: Bet, **kwargs) -> None:
             profile=instance.user.profile,
             points=-1,
             description=UserScores.render_description(-1, f"risk bet {instance.pk}"),
+            kind=UserScores.Kind.BET,
         )
